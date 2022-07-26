4b4cdd015e8c20316f883889b2abbe24f0b6166d
c.giacometti <camilla.giacometti@gmail.com>
2022-07-26 20:09:05 -0300

    versão final

--- linkedriven.sql
+++ linkedriven.sql
@@ -1,7 +1,8 @@
 /* QUESTÃO 1 */
 SELECT users.id, users.name, cities.name AS city
 FROM users
-JOIN cities ON users."cityId" = cities.id;
+JOIN cities ON users."cityId" = cities.id
+WHERE cities.name = 'Rio de Janeiro';
 
 /*QUESTÃO 2 */
 SELECT testimonials.id, 
@@ -9,8 +10,8 @@ tw.name AS writer,
 tr.name AS recipient, 
 testimonials.message 
 FROM testimonials 
-JOIN users tw ON tw.id = testimonials."writerId" 
-JOIN users tr ON tr.id = testimonials."recipientId"
+JOIN users as tw ON tw.id = testimonials."writerId" 
+JOIN users as tr ON tr.id = testimonials."recipientId"
 ORDER BY id ASC;
 
 /*QUESTÃO 3*/
@@ -18,7 +19,7 @@ SELECT educations."userId" AS id,
 users.name AS name, 
 courses.name AS course, 
 schools.name AS school, 
-educations."endDate" AS endDate 
+educations."endDate"
 FROM educations 
 JOIN users ON users.id = educations."userId" 
 JOIN courses ON courses.id = educations."courseId" 
